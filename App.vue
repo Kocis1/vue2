183bd086ea5e62f48b74ca5e2733d938ca83ea0a
Koch <kocisov3@gmail.com>
2024-12-15 23:30:22 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/gallery">Gallery</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  nav
+  {
+    text-align: center;
+  }
+  nav *
+  {
+    border: solid 1px black;
+    background-color: aquamarine;
+  }
+</style>
---
+++ Gallery.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Gallery</h1>
+    <img src="https://i.pinimg.com/736x/fa/7d/7d/fa7d7d0dd96896a6d47f06fd2817ffa7.jpg">
+    <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+    <button>Лайк</button>
+</template>
+<script>
+    export default
+    {
+        data()
+        {
+            return
+            {
+                likes:0;
+            }
+        }
+    }
+</script>
+<style>
+</style>
